bd8c3b99c91c17d0d01b1a8ecbc49e0366e29398
nova togatorop <nova@roadbeats.com>
2020-02-05 00:53:47 +0100

    Rescue gently from Pundit::NotAuthorizedError

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,53 @@
 class PlacesController < ApplicationController
+  skip_before_action :authenticate_user!
+  before_action :set_place, only: [ :show, :edit, :update, :destroy ]
+
+  def index
+    @places = policy_scope(Place)
+  end
+
+  def show
+    @places
+  end
+
+  def new
+    @place = Place.new
+    authorize @place
+
+  end
+
+  def create
+    @place = Place.new(place_params)
+    @place.user = current_user
+    authorize @place
+    if @place.save
+      redirect_to @place
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @place.update(place_params)
+      redirect_to @place
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @place.destroy
+    redirect_to places_url
+  end
+
+  private
+
+  def set_place
+    @place = Place.find(params[:id])
+    authorize @place
+  end
+
+  def place_params
+    params.require(:place).permit(:name, :address, :user_id)
+  end
 end
