a1b47fb11e6fef4735c9dff51b4f53925b2b474b
nova togatorop <nova@roadbeats.com>
2020-02-04 17:20:23 +0100

    Add attributes to User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  private
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :email,:password])
+  end
 end
---
+++ db/migrate/20200204161117_add_username_to_user.rb
@@ -0,0 +1,10 @@
+class AddUsernameToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :latitude, :float
+    add_column :users, :longitude, :float
+    add_column :users, :admin, :boolean, default: false
+  end
+end
