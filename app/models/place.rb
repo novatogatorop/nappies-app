4402f869349d542a4cf85c56947e49d9a24c7212
nova togatorop <nova@roadbeats.com>
2020-02-05 14:21:32 +0100

    Style search input

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,13 @@
 class Place < ApplicationRecord
   belongs_to :user
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    against: [ :name, :address ],
+    # associated_against: {
+    #   type: [ :name ]
+    # },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
