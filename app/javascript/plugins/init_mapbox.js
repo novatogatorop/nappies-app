0f552a9260d990585c10e75fdc29798bc71e4688
nova togatorop <nova@roadbeats.com>
2020-02-13 00:01:37 +0100

    Update css

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -8,6 +8,7 @@ const initMapbox = () => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
     map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+    map.scrollZoom.disable();
   };
 
   if (mapElement) { // only build a map if there's a div#map to inject into
