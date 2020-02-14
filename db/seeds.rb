a7dc005608a79081d83f47c3afd35b8e42422355
nova togatorop <nova@roadbeats.com>
2020-02-14 15:58:52 +0100

    Add alternative image for place photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying all records of users, and places..."
+
+User.destroy_all
+
+
+user1 = User.create({
+  username: "Lala",
+  first_name: "Lala",
+  last_name: "Lala",
+  email: "lala@example.com",
+  password: 123123,
+  admin: true
+})
+
+puts "Users seeded! #{ User.count}"
+
+place1 = Place.new({
+  name: "Populus Coffe",
+  address: "Maybachufer 20, Berlin",
+  user: user1
+  # photo1: 'place/place-cocolo/1.JPG',
+  # photo2: 'place/place-cocolo/1.JPG',
+  # photo3: 'place/place-cocolo/1.JPG',
+  # photo4: 'place/place-cocolo/1.JPG',
+  # photo5: 'place/place-cocolo/1.JPG'
+})
+place1.save
+
+puts "Places seeded! #{ Place.count}"
+puts 'Finished!'
