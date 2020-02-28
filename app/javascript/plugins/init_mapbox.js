2eb547c90d5077ec314bd1dd60118adf26b5105d
nova togatorop <nova@roadbeats.com>
2020-02-28 13:01:35 +0100

    Add custom marker

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,5 +1,25 @@
 import mapboxgl from 'mapbox-gl';
 
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
+    // Create a HTML element for your custom marker
+  const element = document.createElement('div');
+  element.className = 'marker';
+  element.style.backgroundImage = `url('${marker.image_url}')`;
+  element.style.backgroundSize = 'contain';
+  element.style.width = '40px';
+  element.style.height = '40px';
+
+  // Pass the element as an argument to the new marker
+  new mapboxgl.Marker(element)
+    .setLngLat([ marker.lng, marker.lat ])
+    .setPopup(popup) // add this
+    .addTo(map);
+  });
+};
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
@@ -18,35 +38,11 @@ const initMapbox = () => {
       style: 'mapbox://styles/mapbox/navigation-guidance-day-v4'
     });
     const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .addTo(map);
-    });
     addMarkersToMap(map, markers)
     fitMapToMarkers(map, markers)
     // map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken, mapboxgl: mapboxgl }));
   }
 };
 
-const addMarkersToMap = (map, markers) => {
-  markers.forEach((marker) => {
-    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
-
-    // Create a HTML element for your custom marker
-  const element = document.createElement('div');
-  element.className = 'marker';
-  element.style.backgroundImage = `url('${marker.image_url}')`;
-  element.style.backgroundSize = 'contain';
-  element.style.width = '25px';
-  element.style.height = '25px';
-
-  // Pass the element as an argument to the new marker
-  new mapboxgl.Marker()
-    .setLngLat([ marker.lng, marker.lat ])
-    .setPopup(popup) // add this
-    .addTo(map);
-  });
-};
 
 export { initMapbox };
