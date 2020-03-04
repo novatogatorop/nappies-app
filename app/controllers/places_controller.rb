140969d435cc441e2803b802c17618085b6bd345
nova togatorop <nova@roadbeats.com>
2020-03-04 09:15:00 +0100

    Add map pin small size

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -27,7 +27,7 @@ class PlacesController < ApplicationController
         lng: place.longitude,
         infoWindow: render_to_string(partial: "map_box", locals: { place: place }),
         # infoWindow: { content: render_to_string(partial: "/places/map_box", locals: { place: place }) },
-        image_url: helpers.asset_url('map-pin-nappies-border.png')
+        image_url: helpers.asset_url('map-pin-nappies-border-small.png')
       }
     end
   end
@@ -36,7 +36,7 @@ class PlacesController < ApplicationController
     @markers = Array.new
     @markers << { lat: @place.latitude,
                   lng: @place.longitude,
-                  image_url: helpers.asset_url('map-pin-nappies-border.png')
+                  image_url: helpers.asset_url('map-pin-nappies-border-small.png')
                  }
   end
 
