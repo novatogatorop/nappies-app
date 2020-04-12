6fa89fd5b366d773a5f96c276a9fcdf00c270064
nova togatorop <nova@roadbeats.com>
2020-04-12 10:45:02 +0200

    Add ransack gem

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,27 +2,11 @@ class PlacesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_place, only: [ :show, :edit, :update, :destroy ]
 
+
   def index
     @places = policy_scope(Place)
-    @types_all = Type.pluck(:id)
-    @types = []
-    @text_search = params[:search]
-
-    if params[:type].present?
-      @types = params[:type].empty? ? @types_all : params[:type]
-    else
-      @types = @types_all
-    end
-
-    if params[:diaper].present?
-    end
-
-    if @text_search.present?
-      @places = Place.global_search(params[:search]).where(type: @types).where(diaper: params[:diaper])
-    else
-      @places = Place.all
-      @result = "No Result"
-    end
+    @query = Place.ransack(params[:query])
+    @places = @query.result
 
     @geo_places = @places.geocoded
     @markers = @places.map do |place|
@@ -35,6 +19,43 @@ class PlacesController < ApplicationController
       }
     end
   end
+
+
+
+
+  # def index
+  #   @places = policy_scope(Place)
+  #   @types_all = Type.pluck(:id)
+  #   @types = []
+  #   @text_search = params[:search]
+
+  #   if params[:type].present?
+  #     @types = params[:type].empty? ? @types_all : params[:type]
+  #   else
+  #     @types = @types_all
+  #   end
+
+  #   if params[:diaper].present?
+  #   end
+
+  #   if @text_search.present?
+  #     @places = Place.global_search(params[:search]).where(type: @types).where(diaper: params[:diaper])
+  #   else
+  #     @places = Place.all
+  #     @result = "No Result"
+  #   end
+
+  #   @geo_places = @places.geocoded
+  #   @markers = @places.map do |place|
+  #     {
+  #       lat: place.latitude,
+  #       lng: place.longitude,
+  #       infoWindow: render_to_string(partial: "map_box", locals: { place: place }),
+  #       # infoWindow: { content: render_to_string(partial: "/places/map_box", locals: { place: place }) },
+  #       image_url: helpers.asset_url('map-pin-nappies-border-small.png')
+  #     }
+  #   end
+  # end
 
   def show
     @facility = Facility.new
