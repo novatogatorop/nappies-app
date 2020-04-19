f459b72e85966dd03e909e06bdd3326abed91cb3
nova togatorop <nova@roadbeats.com>
2020-04-19 13:27:53 +0200

    Move user name next to profile photo

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  # root to: 'places#index'
-  get 'search', to: 'places#index'
+  root to: 'places#index'
+  # get 'search', to: 'places#index'
 
   resources :places
-  # resources :places do
-  #   resources :place_facilities, only: [:new, :create]
-  # end
-  # resources :place_facilities, only: [:destroy]
-  # resources :types, only: [:index, :new, :create, :edit, :update, :destroy]
-  # resources :facilities, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :places do
+    resources :place_facilities, only: [:new, :create]
+  end
+  resources :place_facilities, only: [:destroy]
+  resources :types, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :facilities, only: [:index, :new, :create, :edit, :update, :destroy]
 end
