f0f79374ec5f339c62f4cf48d8ccf43a769f4a7e
nova togatorop <nova@roadbeats.com>
2020-04-26 16:23:33 +0200

    Change config default url to nappiesapp.com

--- db/migrate/20200426125757_remove_diaper_from_places.rb
+++
@@ -1,8 +0,0 @@
-class RemoveDiaperFromPlaces < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :places, :diaper
-    remove_column :places, :toy
-    remove_column :places, :high_chair
-    remove_column :places, :play_area
-  end
-end
--- db/migrate/20200426130015_add_diaper_to_places.rb
+++
@@ -1,8 +0,0 @@
-class AddDiaperToPlaces < ActiveRecord::Migration[5.2]
-  def change
-    add_column :places, :diaper, :boolean, default: false
-    add_column :places, :toy, :boolean, default: false
-    add_column :places, :high_chair, :boolean, default: false
-    add_column :places, :play_area, :boolean, default: false
-  end
-end
