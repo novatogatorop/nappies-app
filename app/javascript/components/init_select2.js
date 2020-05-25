8235ec7542586bbc26be4b0781995a3e9ccd96c2
nova togatorop <nova@roadbeats.com>
2020-05-25 14:12:45 +0200

    install select2 gem

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -1,11 +1,8 @@
-// // imports
-// import $ from 'jquery';
-// import 'select2';
+import $ from 'jquery';
+import 'select2';
 
-// // function definitions
-// const initSelect2 = () => {
-//   $('.select2').select2(); // (~ document.querySelectorAll)
-// };
+const initSelect2 = () => {
+  $('.select2').select2();
+};
 
-// // exports (~ public interface)
-// export { initSelect2 };
+export { initSelect2 };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,8 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 import { initMapbox } from '../plugins/init_mapbox';
 import { searchPopup, closePopup } from '../components/search_mobile';
 import { dropdownPopup, dropdownClose } from '../components/dropdown_mobile';
+import { initSelect2 } from '../components/init_select2';
+
 
 
 
@@ -14,3 +16,4 @@ searchPopup();
 closePopup();
 dropdownPopup();
 dropdownClose();
+initSelect2();
