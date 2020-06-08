e720f76e6dd65ac8415cda42454e10c6f907b72c
nova togatorop <nova@roadbeats.com>
2020-06-08 13:53:20 +0200

    modify navbar dropdown js

--- app/javascript/components/dropdown_mobile.js
+++ app/javascript/components/dropdown_mobile.js
@@ -5,12 +5,14 @@ const dropdownPopup = () => {
   const dropdownList =document.querySelector(".dropdown-list")
   const searchButton =document.querySelector(".search-button-navbar")
   const navbar =document.querySelector(".navbar-mobile")
+  const nappiesCon =document.querySelector(".nappies-container")
   logoOpen.addEventListener('click', (event) => {
     dropdownList.style.display = "block";
     searchButton.style.display = "none";
     logoClose.style.marginBottom = "16px";
     logoClose.style.display = "block";
     logoOpen.style.display = "none";
+    nappiesCon.style.display = "none";
     dropdownMobile.style.width = "100%";
     dropdownMobile.style.height = "100vh";
     navbar.style.height = "100%";
@@ -24,10 +26,12 @@ const dropdownClose = () => {
   const dropdownList =document.querySelector(".dropdown-list")
   const searchButton =document.querySelector(".search-button-navbar")
   const navbar =document.querySelector(".navbar-mobile")
+  const nappiesCon =document.querySelector(".nappies-container")
   logoClose.addEventListener('click', (event) => {
     dropdownList.style.display = "none";
     logoClose.style.display = "none";
     logoOpen.style.display = "block";
+    nappiesCon.style.display = "flex";
     logoOpen.style.marginBottom = "0px";
     searchButton.style.display = "block";
     dropdownMobile.style.width = "";
--- app/javascript/components/search_mobile.js
+++ app/javascript/components/search_mobile.js
@@ -5,10 +5,12 @@ const searchPopup = () => {
   const dropdown =document.querySelector(".dropdown-mobile")
   const searchMobile =document.querySelector(".search-mobile")
   const navbar =document.querySelector(".navbar-mobile")
+  const nappiesCon =document.querySelector(".nappies-container")
   searchButton.addEventListener('click', (event) => {
     searchPopup.style.display = "block";
     searchButton.style.display = "none";
     searchButtonX.style.display = "block";
+    nappiesCon.style.display = "none";
     searchButtonX.style.marginBottom = "16px";
     dropdown.style.display = "none";
     searchMobile.style.width = "100%";
@@ -24,11 +26,13 @@ const closePopup = () => {
   const dropdown =document.querySelector(".dropdown-mobile")
   const searchMobile =document.querySelector(".search-mobile")
   const navbar =document.querySelector(".navbar-mobile")
+  const nappiesCon =document.querySelector(".nappies-container")
   searchButtonX.addEventListener('click', (event) => {
     searchPopup.style.display = 'none';
     searchButtonX.style.display = "none";
     searchButton.style.display = "block";
     dropdown.style.display = "block";
+    nappiesCon.style.display = "flex";
     searchMobile.style.width = "";
     searchMobile.style.height = "0";
     navbar.style.height = "10%";
