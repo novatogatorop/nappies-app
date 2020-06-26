e05a7453b8dbf0f6faa18ffb7aaabce49e41d8c9
nova togatorop <nova@roadbeats.com>
2020-06-26 09:45:14 +0200

    remove clickable

--- app/javascript/components/clickable.js
+++ app/javascript/components/clickable.js
@@ -3,19 +3,24 @@ const highChair = document.querySelector(".nav-high-chair");
 const toy = document.querySelector(".nav-toy");
 const playArea = document.querySelector(".nav-play-area");
 const inputChangingTable = document.querySelector(".input-changing-table").onclick = function() { navbarBottomClickable() };
-const clicks = document.querySelectorAll(".clickable");
+// const clicks = document.querySelectorAll(".clickable");
 
 
 
+
+// const iconClickable = () => {
+//   clicks.forEach((click) => {
+//     click.addEventListener("click", (event) => {
+//       click.classList.toggle("active");
+//     });
+//   });
+// };
 
 const iconClickable = () => {
-  clicks.forEach((click) => {
-    click.addEventListener("click", (event) => {
-      click.classList.toggle("active");
-    });
+  clicks.addEventListener("click", (event) => {
+    clicks.classList.toggle("active");
   });
 };
-
 
 
 
