c8c1ef393a27d2b290ccd8a52de816c8d5267552
nova togatorop <nova@roadbeats.com>
2020-06-26 15:11:12 +0200

    try to implement toggle for navbar bottom icon

--- app/javascript/components/clickable.js
+++ app/javascript/components/clickable.js
@@ -1,12 +1,13 @@
-const changingTable = document.querySelector(".nav-changing-table");
-const highChair = document.querySelector(".nav-high-chair");
-const toy = document.querySelector(".nav-toy");
-const playArea = document.querySelector(".nav-play-area");
-const inputChangingTable = document.querySelector(".input-changing-table").onclick = function() { navbarBottomClickable() };
-// const clicks = document.querySelectorAll(".clickable");
+const box = document.querySelector(".navbar-bottom-box");
+const items = document.querySelector(".navbar-bottom-items");
 
-
-
+const iconClickable = () => {
+  items.forEach(el => {
+    el.addEventListener('click', function(){
+      box.querySelector('.active').classList.remove('.active')
+    });
+  });
+};
 
 // const iconClickable = () => {
 //   clicks.forEach((click) => {
@@ -15,13 +16,6 @@ const inputChangingTable = document.querySelector(".input-changing-table").oncli
 //     });
 //   });
 // };
-
-const iconClickable = () => {
-  clicks.addEventListener("click", (event) => {
-    clicks.classList.toggle("active");
-  });
-};
-
 
 
 
