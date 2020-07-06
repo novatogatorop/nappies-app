f5eaf4f8b29caabe9f0bc4464c5f3a2a955ed257
nova togatorop <nova@roadbeats.com>
2020-07-06 10:21:39 +0200

    hide navbar mobile when scroll up and hide it when scroll down

---
+++ app/javascript/components/navbar_mobile.js
@@ -0,0 +1,17 @@
+const navMobile = document.querySelector(".navbar-mobile")
+var lastScrollTop = 0
+
+
+const navbarMobile = () => {
+  window.addEventListener("scroll", function() {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop){
+      navMobile.style.top="-60px";
+    } else {
+      navMobile.style.top="0";
+    }
+    lastScrollTop = scrollTop
+  });
+};
+
+export { navbarMobile };
