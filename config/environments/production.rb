66a2025713b224f5a5afaa94c6da85a2553fbf8c
nova togatorop <nova@roadbeats.com>
2020-07-21 15:03:36 +0200

    add nappiesapp.com in production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'yourdomain.com',
+    :domain => 'nappiesapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
