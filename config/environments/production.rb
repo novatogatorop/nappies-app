4c661cc0ea3e80b67ea88abc7462da84bad4c64e
nova togatorop <nova@roadbeats.com>
2020-07-21 15:27:15 +0200

    update production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,8 +6,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => 'nappies',
+    :password => 'password',
     :domain => 'nappiesapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
