7c3ab9bb0dbd784b4a1f59dc78146c88a0940c59
nova <nova@roadbeats.com>
2020-09-06 23:21:49 +0200

    email confirmation with gmail works

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,13 +2,10 @@ Rails.application.configure do
   config.action_mailer_default_url_options = { host: 'https://nappiesapp.com' }
   Rails.application.routes.default_url_options[:host] = 'https://nappiesapp.com'
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default charset: 'utf-8'
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'gmail.com',
+    domain: 'nappiesapp.com',
     authentication: 'plain',
     enable_starttls_auto: true,
     user_name: ENV['GMAIL_EMAIL'],
